eac97e7168827d28c0e16476073a3e60e7859d6c
talvanio <talvaniufmg@outlook.com>
2026-05-01 23:18:27 -0300

    victory screen

--- game.gd
+++ game.gd
@@ -2,6 +2,10 @@ extends Node2D
 @onready var heartsContainer = $CanvasLayer/HeartsContainer
 @onready var player = $Player
 
+@export var win_time = 30.0
+
+var elapsed_time = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	heartsContainer.set_max_hearts(player.max_health)
@@ -11,4 +15,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	elapsed_time += delta
+
+	if elapsed_time >= win_time:
+		win_game()
+
+func win_game():
+	get_tree().change_scene_to_file("res://victory.tscn")
