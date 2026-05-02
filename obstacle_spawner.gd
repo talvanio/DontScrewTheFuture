9d45ea0e6c35292745b3c6497972fd110575ce29
talvanio <talvaniufmg@outlook.com>
2026-05-01 23:58:50 -0300

    adaptive projectile velocity

--- obstacle_spawner.gd
+++ obstacle_spawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 @export var projectile_scene: PackedScene
 @onready var timer = $Timer
 @export var projectile_frequency: float = 1.0
+@export var projectile_velocity = 700
 
 @onready var lanes = [
 	$Lane1Marker,
@@ -16,13 +17,16 @@ func _ready():
 func shoot():
 	var projectile = projectile_scene.instantiate()
 	var lane = lanes.pick_random()
+	projectile.velocity = projectile_velocity
 	projectile.global_position = lane.global_position
 	get_tree().current_scene.add_child(projectile)
 
 func _on_timer_timeout():
 	shoot()
 
-
+func set_projectile_velocity(value: float):
+	projectile_velocity = value
+	
 func set_projectile_frequency(value: float):
 	projectile_frequency = value
 	timer.wait_time = value
