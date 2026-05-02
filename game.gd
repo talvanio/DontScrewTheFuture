a3d4c35f3c4c0f4fe7b363f6b4093fb01e1fa116
talvanio <talvaniufmg@outlook.com>
2026-05-01 23:27:09 -0300

    simple progress bar

--- game.gd
+++ game.gd
@@ -1,8 +1,9 @@
 extends Node2D
 @onready var heartsContainer = $CanvasLayer/HeartsContainer
 @onready var player = $Player
+@onready var progress_bar = $CanvasLayer/WinProgressBar
 
-@export var win_time = 30.0
+@export var win_time = 10.0
 
 var elapsed_time = 0.0
 
@@ -16,9 +17,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	elapsed_time += delta
+	update_progress_bar()
 
 	if elapsed_time >= win_time:
 		win_game()
+		
+func update_progress_bar():
+	var progress = elapsed_time / win_time
+
+	progress_bar.value = progress * 100
 
 func win_game():
 	get_tree().change_scene_to_file("res://victory.tscn")
