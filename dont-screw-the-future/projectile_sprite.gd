9e93f63ec9abfacbe53b1ffa9c02f50f75ec1ec1
talvanio <talvaniufmg@outlook.com>
2026-05-25 15:05:25 -0300

    removing unused script

--- dont-screw-the-future/projectile_sprite.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-@export var rotation_speed := 100
-@export var vertical_speed : float = 40
-
-
-func _process(delta):
-	rotation_degrees += rotation_speed * delta
-	position.y = position.y + delta * vertical_speed
