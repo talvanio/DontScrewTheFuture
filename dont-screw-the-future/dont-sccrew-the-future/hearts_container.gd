14a40fb8bf4a1bfd4cb1da48dcd589ab4e15a0d8
talvanio <talvaniufmg@outlook.com>
2026-05-25 14:48:16 -0300

    renaming main folder

---
+++ dont-screw-the-future/dont-sccrew-the-future/hearts_container.gd
@@ -0,0 +1,18 @@
+extends HBoxContainer
+
+@onready var heart_gui = preload("res://heart_gui.tscn")
+
+func set_max_hearts(max: int) -> void:
+	for i in range(max):
+		var heart = heart_gui.instantiate()
+		add_child(heart)
+
+func update_hearts(current_health: int):
+
+	var hearts = get_children()
+	
+	for i in range(current_health):
+		hearts[i].set_enabled(true)
+	
+	for i in range(current_health, hearts.size()):
+		hearts[i].set_enabled(false)	
